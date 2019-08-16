5981d61716c7c7c5081ad600bdb98dbf2bab02dc
lishuai <1019023472@qq.com>
2019-08-16 15:09:22 +0800

    第一次上传

---
+++ student_manger/src/main/java/com/huayu/student_manger/StudentMangerApplication.java
@@ -0,0 +1,13 @@
+package com.huayu.student_manger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentMangerApplication.class, args);
+    }
+
+}
